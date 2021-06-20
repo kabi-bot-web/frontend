0e9a347ad05e6bbc754c55298ad97515e755ed46
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-06-20 17:30:24 +0800

    add home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.sass';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 import Home from './components/home';
 
 function App() {
@@ -9,6 +10,14 @@ function App() {
         <ul id="navbar">
           <li id="Title">
             <Link to="/">KABI</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li id="Login">
+            <Button variant="contained" size="medium" color="primary" href="/user/authorize" >
+              Login
+            </Button>
           </li>
         </ul>
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,28 @@
+import kabiImg from './imgs/kabi.png';
+import Button from '@material-ui/core/Button';
+import './sass/home.sass';
 
 const Home = () => {
     return (
         <div id="Home">
-            ww
+            <img src={kabiImg} alt="" />
+            <div className="JoinButtons">
+                <Button 
+                    variant="outlined" 
+                    color="primary" 
+                    disableElevation
+                    href="https://discord.com/api/oauth2/authorize?client_id=848687767507828787&permissions=8&redirect_uri=http%3A%2F%2F127.0.0.1%3A8080%2Fuser%2Fauthorize&scope=bot"
+                >
+                    Add to Server
+                </Button>
+                <Button
+                    variant="outlined"
+                    color="primary"
+                    disableElevation href="/"
+                >
+                    Join our Server
+                </Button>
+            </div>
         </div>
     );
 }
