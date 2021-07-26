aab413715854e2b3559275a5ec939a810b431fee
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-07-27 01:20:47 +0800

    coding style !!!

--- src/components/servers.jsx
+++ src/components/servers.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import './sass/dashboard.sass';
 import Guilds from './guilds';
 import { userAPI } from "./js/api";
