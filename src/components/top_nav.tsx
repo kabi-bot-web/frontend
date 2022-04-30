f9ae6fdf472a275bcd4698a8aa65a0065ee8bfce
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-05-01 02:58:35 +0800

    top nav add search

--- src/components/top_nav.tsx
+++ src/components/top_nav.tsx
@@ -1,27 +1,33 @@
 import { FC } from "react";
-import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Top_navigation: FC = () => {
     return (
-        <div className="h-16 w-full pl-72 bg-gray-800 flex grow">
-            <div className="h-full w-full">
+        <div className="h-16 w-full pl-72 bg-zinc-800 flex grow">
+            <div className="h-full w-full flex items-center">
                 <Paper
+                    className="hover:w-96 hover:animate-search-input hover:rounded-md animate-search-input2 focus-within:animate-search-input focus-within:rounded-md focus-within:w-96"
                     component="form"
-                    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+                    sx={{
+                        p: '2px 4px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        width: 50,
+                        backgroundColor: 'rgb(24, 26, 27)',
+                        transition: 'all .5s',
+                        borderRadius: '50%'
+                    }}
                 >
-                    <IconButton sx={{ p: '10px' }} aria-label="menu">
-                        <MenuIcon />
+                    <IconButton sx={{ p: '10px', color: '#ffffff' }} aria-label="menu">
+                        <SearchIcon />
                     </IconButton>
                     <InputBase
-                        sx={{ ml: 1, flex: 1 }}
-                        placeholder="Search Google Maps"
+                        sx={{ ml: 1, flex: 1, color: '#ffffff' }}
+                        placeholder="Search Setting"
                         inputProps={{ 'aria-label': 'search google maps' }}
                     />
                 </Paper>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                'search-input': 'searchInput .5s',
+                'search-input2': 'searchInput2 .5s'
+            },
+            keyframes: {
+                searchInput: {
+                    '0%': { borderRadius: '50%' },
+                    '10%': { borderRadius: '6px' },
+                },
+                searchInput2: {
+                    '0%, 80%': { borderRadius: '6px' },
+                    '100%': { borderRadius: '50%' },
+                }
+            }
+        },
     },
     plugins: [],
 }
