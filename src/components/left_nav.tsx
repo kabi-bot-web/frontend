a83332d49e5c100f505fd67cfbae3b97eea72dee
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-05-05 21:17:46 +0800

    nav RWD

--- src/components/left_nav.tsx
+++ src/components/left_nav.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -17,7 +18,7 @@ const Left_navigation: FC<Left_navigation> = ({ navFull, setNavFull }) => {
 
   return (
     <div
-      className={`left-0 top-0 h-full fixed z-10 transition-all duration-500 ${navFull ? 'w-64' : 'w-20'}`}
+      className={`left-0 top-0 h-full fixed z-10 transition-all duration-500 ${navFull ? 'sm:w-64 w-full' : 'w-20'}`}
       style={{ backgroundColor: 'rgb(26, 28, 30)' }}
     >
       <div className={`h-16 flex items-center ${navFull ? 'justify-end pr-5' : 'justify-center'}`}>
