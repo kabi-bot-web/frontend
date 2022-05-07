c80970de6e4b41512ebccdaed608766ebbb25037
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-05-07 22:17:47 +0800

    add desktop auto open nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Top_navigation from './components/top_nav';
 import Left_navigation from './components/left_nav';
 import Content from './components/content';
 import { Author } from './data/author';
 
 function App() {
-  const [navFull, setNavFull] = useState(true);
+  const [navFull, setNavFull] = useState(false);
   const [author, setAuthor] = useState<Author>({ avatar: 'https://cdn.discordapp.com/avatars/458988300418416640/c3dd597ba056c3552f3c19453b71007e.jpg?size=1024' });
+
+  // desktop open nav
+  useEffect(() => {
+    if (document.body.clientWidth > 640) setNavFull(true);
+  });
 
   return (
     <div className="w-full h-full">
